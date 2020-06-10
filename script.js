eaf412af9884e993520112391628ce4c88bec8f6
termuxuser01 <termuxuser01@gmail.com>
2020-06-10 23:18:01 +0000

    add color changer in js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ const colors = [
 const btn = document.getElementById('btn');
 const color = document.getElementById('color');
 
+btn.addEventListener('click', function(){
+    // get random number
+    const randomNumber = getRN();
+    // set background to random color from array
+    document.body.style.backgroundColor = colors[randomNumber];
+    // display the color
+    color.textContent = colors[randomNumber];
+});
+
+function getRN(){
+  return Math.floor(Math.random() * colors.length)
+}
